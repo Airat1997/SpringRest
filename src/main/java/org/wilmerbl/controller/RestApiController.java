aa9667decf2bdcace6455eea6e51128fbfe74577
Wilmer Blossom <wilmerbl@ge-i5.kzn.21-school.ru>
2024-06-09 17:07:21 +0300

    add put and delete request

--- src/main/java/org/wilmerbl/controller/RestApiController.java
+++ src/main/java/org/wilmerbl/controller/RestApiController.java
@@ -1,12 +1,15 @@
 package org.wilmerbl.controller;
 
 
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,6 +46,21 @@ public class RestApiController {
     ResponseEntity<Product> postProduct(@RequestBody Product product) {
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Product> putProduct(@PathVariable UUID id, @RequestBody Product product) {
+        return !productService.existsById(id) ? new ResponseEntity<>(
+                productService.save(product), HttpStatus.CREATED)
+                : new ResponseEntity<>(productService.save(product), HttpStatus.OK);
+    }
+
+    @DeleteMapping("{id}")
+    ResponseEntity<Void> deleteProduct(@PathVariable UUID id){
+        return productService.deleteById(id);
+    }
+
+
+
 
 
 
--- src/main/java/org/wilmerbl/service/ProductService.java
+++ src/main/java/org/wilmerbl/service/ProductService.java
@@ -1,6 +1,8 @@
 package org.wilmerbl.service;
 
+import java.util.UUID;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.wilmerbl.filter.ProductSpecification;
 import org.wilmerbl.model.Product;
@@ -19,21 +21,35 @@ public class ProductService {
         return productRepository.findAll(spec);
     }
 
-    public Product save(Product product){
+    public ResponseEntity<Void> deleteById(UUID id) {
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    public boolean existsById(UUID id) {
+        return productRepository.findById(id).isPresent();
+    }
+
+    public Product save(Product product) {
         return productRepository.save(product);
     }
-    public Iterable<Product> findAllFiltered(String name,String description, Double price, Boolean productAvailability) {
+
+    public Iterable<Product> findAllFiltered(String name, String description, Double price,
+            Boolean productAvailability) {
         Specification<Product> spec = Specification.where(null);
-        if (name!= null) {
+        if (name != null) {
             spec = spec.and(ProductSpecification.hasName(name));
         }
-        if (description!= null) {
+        if (description != null) {
             spec = spec.and(ProductSpecification.hasDescription(description));
         }
-        if (price!= null) {
+        if (price != null) {
             spec = spec.and(ProductSpecification.hasPrice(price));
         }
-        if (productAvailability!= null) {
+        if (productAvailability != null) {
             spec = spec.and(ProductSpecification.hasProductAvailability(productAvailability));
         }
         return productRepository.findAll(spec);
